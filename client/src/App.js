266bd60b2a024b70e0441bd654c6dc8766faabd4
Jonathan Huang <jonathan95129@gmail.com>
2022-06-14 19:13:36 -0400

    googel docs complete, may need formatting to copy website doc saving, etc

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import TextEditor from "./TextEditor.js"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom"
+import { v4 as uuidV4 } from 'uuid'
+
+function App() {
+  return <Router>
+    <Routes>
+
+      <Route path='/' exact element={<Navigate to= {`documents/${uuidV4()}`}/>}>
+
+      </Route>
+      <Route path='/documents/:id' element={<TextEditor/>} >
+      </Route>
+    </Routes>
+  </Router>
+}
+
+export default App;
